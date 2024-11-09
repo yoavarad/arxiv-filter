a731a1c7890338a540ccb5febdee2517583cd0a2
yoavarad <yoavarad10@gmail.com>
2024-11-09 11:49:01 +0200

    csv write -> df.to_csv

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pickle
 from datetime import datetime, timezone, tzinfo
 
 import arxiv
+import pandas as pd
 from tqdm import tqdm
 
 client = arxiv.Client()
@@ -32,7 +33,7 @@ while True:
             desc="Processing batch",
         ):
             last_r = r
-            if r.updated >= start_of_week and "cs.CV" in r.categories:
+            if r.published >= start_of_week and "cs.CV" in r.categories:
                 batch_papers.append(r)
     except arxiv.UnexpectedEmptyPageError as e:
         logger.warning(e)
@@ -41,29 +42,22 @@ while True:
 
     this_week_papers.extend(batch_papers)
 
-    if last_r is not None and last_r.updated >= start_of_week:
+    if last_r is not None and last_r.published >= start_of_week:
         offset += batch_size
     else:
         break
 
-print(f"Found {len(this_week_papers)} papers submitted this week.")
 
-with open("this_week_papers_9_11_2024.csv", "w") as csvfile:
-    fieldnames = ["index", "id", "title", "link"]
-    writer = csv.DictWriter(
-        csvfile,
-        fieldnames=fieldnames,
-        dialect=csv.unix_dialect,
-    )
-    writer.writeheader()
-    writer.writerows(
-        [
-            {
-                "index": idx,
-                "id": paper.get_short_id(),
-                "title": paper.title,
-                "link": paper.pdf_url,
-            }
-            for idx, paper in enumerate(this_week_papers)
-        ]
-    )
+processed = {}
+for paper in this_week_papers:
+    processed[paper.get_short_id().split("v")[0]] = {
+        "id": paper.get_short_id(),
+        "title": paper.title,
+        "link": paper.pdf_url,
+        "published": paper.published.astimezone(timezone.utc).strftime(
+            "%Y-%m-%d %H:%M:%S"
+        ),
+    }
+processed_df = pd.DataFrame(processed.values())
+print(f"Found {len(processed_df)} papers submitted this week.")
+processed_df.to_csv("this_week_papers_9_11_2024.csv", index=True)
